6a81a11d5285d3611f3bf424b49aa239287cbde9
SHuRiKeN <shuriken1812@proton.me>
2023-12-02 08:43:31 +0530

    Implement GUI, seperate CLI with a cli flag

---
+++ cli/start.py
@@ -0,0 +1,19 @@
+import requests
+
+import setup.location as location
+import setup.method as method
+
+
+def run_setup():
+    location.get_location_response()
+    method.get_method_response()
+
+    url = "http://api.aladhan.com/v1/timingsByCity"
+    params = {"city": location.city, "country": location.country}
+
+    response = requests.get(
+        url,
+        params,
+    )
+    print(f"response code:{response.status_code}")
+    print(f"response:{response.text}")
--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-import requests
-
-import setup.location as location
-import setup.method as method
-
-
-def run_setup():
-    location.get_location_response()
-    method.get_method_response()
-
-    url = "http://api.aladhan.com/v1/timingsByCity"
-    params = {"city": location.city, "country": location.country}
-
-    response = requests.get(
-        url,
-        params,
-    )
-    print(f"response code:{response.status_code}")
-    print(f"response:{response.text}")
-
+import sys
+import cli.start as cli
+import gui.start as gui
 
 if __name__ == "__main__":
-    run_setup()
+    if len(sys.argv) > 1 and sys.argv[1] == "cli":
+        cli.run_setup()
+    else:
+        gui.run()
