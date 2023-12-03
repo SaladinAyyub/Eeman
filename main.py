e80d51ab5c388e1e48ad3ea784161c2d935dabe7
SHuRiKeN <shuriken1812@proton.me>
2023-12-03 06:25:50 +0530

    separate GUI repo

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+from PyQt6.QtCore import QSize, QCoreApplication
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
 import sys
-import cli.start as cli
-import gui.start as gui
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Eeman")
+        self.setFixedSize(QSize(500, 300))
+        self.setup_ui()
+
+    def setup_ui(self):
+        btn = QPushButton("Yes", self)
+        btn.resize(100, 50)
+        btn.move(50, 200)
+        btn.clicked.connect(QCoreApplication.instance().quit)
+
+
+def run():
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    app.exec()
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "cli":
-        cli.run()
-    else:
-        gui.run()
+    run()
