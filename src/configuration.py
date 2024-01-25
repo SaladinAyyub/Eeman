0649f47cd08137fecf4026e7c5d28000db08145b
SHuRiKeN <shuriken1812@proton.me>
2024-01-25 22:22:57 +0530

    copy from fallback path to user path

--- src/configuration.py
+++ src/configuration.py
@@ -2,14 +2,24 @@ from configparser import ConfigParser
 from pathlib import Path
 
 from gi.repository import GLib
+import shutil
+import os
 
 config = ConfigParser()
 
-config_path = Path(GLib.get_user_data_dir(), ".", "config.ini")
+datadir = Path(GLib.get_user_data_dir() + "/eeman")
+user_path = Path(GLib.get_user_data_dir(), "eeman", "config.ini")
+fallback_path = Path(__file__).parent.resolve() / "config.ini"
+
+if not os.path.exists(datadir):
+    os.makedirs(datadir)
+
+shutil.move(fallback_path, user_path)
+config_path = user_path
 
 if not config_path.is_file():
     # Fall back to default config file
-    config_path = Path(__file__).parent.resolve() / "config.ini"
+    config_path = fallback_path
 
 
 def get_conf():
